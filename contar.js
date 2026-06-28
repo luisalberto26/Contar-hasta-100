291fd86b729c4dba7005846f15c6d0b965cafd5b
luisalberto26 <56483785+luisalberto26@users.noreply.github.com>
2026-06-28 14:03:07 -0400

    Add function to count and log even numbers up to 100
    Implement a loop to display even numbers from 1 to 100.

---
+++ contar.js
@@ -0,0 +1,12 @@
+//Se utiliza el ciclo for para contar los números hasta 100. 
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+//Mostrar números pares del 1 al 100.
+
